17e19fd236cea124549e7e6715f07a05323abdaa
N-S-practice-code <nico.ehs05@gmail.com>
2018-09-05 13:28:02 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,6 +6,8 @@ def batch_badge_creator(array)
   
 end
 
-def batch_badge_creator(array)
-  
+def assign_rooms(array)
+  array.each_with_index do |name|
 end
+
+assign_rooms(["Steve"])).to eq(["Hello, Steve! You'll be assigned to room 1!"])
